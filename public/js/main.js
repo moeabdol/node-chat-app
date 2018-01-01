014d72bf3a47fdbafa081327a43726e22726d0bf
moeabdol <mohd.a.saed@gmail.com>
2018-01-01 06:10:03 +0300

    Scroll to bottom if user is close to bottom

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
   var socket = io();
 
+  function scrollToBottom() {
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+      scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    }
+  }
+
   socket.on('connect', function() {
     console.log('Connected to server');
   });
@@ -20,6 +36,7 @@ $(document).ready(function() {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
   });
 
   socket.on('newLocationMessage', function(message) {
@@ -33,6 +50,7 @@ $(document).ready(function() {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
   });
 
   var messageTextBox = $('[name=message]');
